be56e91bb171e74a6e354f93b56256c436fe0702
bevanwsjones <bevanwsjones@gamil.com>
2026-09-07 13:08:26 +0100

    - added termination flag for queue.

--- src/queues/include/concurrent_queue.hpp
+++ src/queues/include/concurrent_queue.hpp
@@ -4,6 +4,7 @@
 #include <condition_variable>
 #include <queue>
 #include <vector>
+#include <optional>
 
 namespace cheetah
 {
@@ -16,30 +17,43 @@ class ConcurrentQueue
     ConcurrentQueue() = default;
     ~ConcurrentQueue() = default;
 
+
     void push(const T& value) {
         std::unique_lock lg(mut_que);
+        // assert not done
         queue.push_back(value);
         cv_lock.notify_one();        
     };
 
     void push(const std::vector<T>& value){
         std::unique_lock lg(mut_que);
+        // assert not done
         for(const auto& val : value){
             queue.push_back(val);
         }
         cv_lock.notify_one();
     };
 
-    T front_pop() {
+    void set_done() {
+        std::scoped_lock lg(mut_que);
+        done = true;
+        cv_lock.notify_all();
+    }
+
+    std::optional<T> front_pop() {
         std::unique_lock lg(mut_que);
-        cv_lock.wait(lg, [this] { return !queue.empty(); });
-        const T value = queue.front();
-        queue.pop_front();
-        return value;
+        cv_lock.wait(lg, [this] { return !queue.empty() | done; });
+        if (!queue.empty()) [[likely]] {
+            const T value = queue.front();
+            queue.pop_front();
+            return value;
+        }
+        return std::nullopt;
     };
 
     private:
 
+    bool done{false};
     std::deque<T> queue;
     std::mutex mut_que;
     std::condition_variable cv_lock;
--- src/queues/test/test_concurrent_queue.cpp
+++ src/queues/test/test_concurrent_queue.cpp
@@ -1,8 +1,35 @@
 #include "concurrent_queue.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+#include <thread>
+#include <vector>
+
+#include <iostream>
+
+void produce_values(cheetah::ConcurrentQueue<int>* queue){
+    std::vector<int> values{1, 2, 3, 4, 5};
+    for(const auto& val : values){
+        queue->push(val);
+    }
+};
+
+void consume_values(cheetah::ConcurrentQueue<int>* queue){
+    while(true){
+        auto maybe_value = queue->front_pop();
+        if(maybe_value) std::cout<<"\n"<<*maybe_value;
+        else break;
+    }
+};
+
 
 TEST_CASE("concurrent_queue compiles", "[concurrent_queue]")
 {
-    cheetah::ConcurrentQueue<int> queue;
+    cheetah::ConcurrentQueue<int> q;
+    std::thread producer(produce_values, &q);
+    std::thread consumer(consume_values, &q);
+
+    producer.join();
+    q.set_done();
+    consumer.join();
+
 }
